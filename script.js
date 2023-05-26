51540164c6d66b48e173c75a907fb14ff244db80
Artur56-Dev <131680937+Artur56-Dev@users.noreply.github.com>
2023-05-26 15:07:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+
+let bodyWidth = document.body.clientWidth;
+console.log(bodyWidth);
+window.addEventListener('load', function () {
+  const hellodiv = document.querySelector('.maindiv .hellodiv');
+  const conteiner1 = document.querySelector('.maincontent .conteiner1');
+  console.log('Высоты div:' + hellodiv.offsetHeight);
+  if (hellodiv.offsetHeight > window.innerHeight) {
+    hellodiv.style.height = "100vh";
+    console.log('div больше и должен обрезатся');
+  }
+  if(this.window.innerWidth > this.window.innerHeight){
+    conteiner1.style.height = "80vh";
+    console.log("Ширина "+this.window.innerWidth + " больше " + "Высоты " + this.window.innerHeight)
+  }
+  else{
+    conteiner1.style.height = "80vw";
+    console.log("Ширина "+ this.window.innerWidth + " меньше " + "Высоты " + this.window.innerHeight)
+  }
+  window.addEventListener("resize", function() {
+    if (window.matchMedia("(orientation: portrait)").matches) {
+      // экран стал вертикальным
+      if(this.window.innerWidth > this.window.innerHeight){
+        conteiner1.style.height = "80vh";
+        console.log("Ширина "+this.window.innerWidth + " больше " + "Высоты " + this.window.innerHeight)
+      }
+      else{
+        conteiner1.style.height = "80vw";
+        console.log("Ширина "+ this.window.innerWidth + " меньше " + "Высоты " + this.window.innerHeight)
+      }
+      console.log('Экран стал вертикальным');
+    } else if (window.matchMedia("(orientation: landscape)").matches) {
+      // экран стал горизонтальным
+      if(this.window.innerWidth > this.window.innerHeight){
+        conteiner1.style.height = "80vh";
+        console.log("Ширина "+this.window.innerWidth + " больше " + "Высоты " + this.window.innerHeight)
+      }
+      else{
+        conteiner1.style.height = "80vw";
+        console.log("Ширина "+ this.window.innerWidth + " меньше " + "Высоты " + this.window.innerHeight)
+      }
+      console.log("Экран стал горизонтальным");
+    }
+  });
+});
+console.log('Видимоя высота:' + window.innerHeight);
+// if (hellodiv.style.height > 100) {
+//   hellodiv.style.height = "100vh";
+//   console.log('div больше и должен обрезатся');
+// }
+
+const div = document.querySelector('.headerdiv');
+const placeholder = document.createElement('div');
+placeholder.style.height = div.offsetHeight + 'px';
+placeholder.style.display = 'none';
+div.parentNode.insertBefore(placeholder, div);
+const threshold = div.offsetTop;
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > threshold) {
+    div.classList.add('fixed-top');
+    placeholder.style.display = 'block'; // show the placeholder element when the div element becomes fixed
+  } else {
+    div.classList.remove('fixed-top');
+    placeholder.style.display = 'none'; // hide the placeholder element when the div element is no longer fixed
+  }
+});
---
+++ резерв/index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Cache-Control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" href="/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Купить недвижимость</title>
+</head>
+
+<body>
+    <div class="headerdiv">
+        <div class="conteiner">
+            <div class="div1">
+                <div class="div1a">
+                    <img src="https://fikusexpert.com/wp-content/uploads/2020/05/Villy-na-kipre.jpg">
+                </div>
+                <div class="div1b">
+                    <p>ДВоадваджов</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
