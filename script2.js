9aeacfe63119b44f9d2f4000e932b084ab77d817
Artur56-Dev <131680937+Artur56-Dev@users.noreply.github.com>
2023-05-30 15:28:43 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,50 +1,50 @@
-const div = document.querySelector('.headerdiv');
-const placeholder = document.createElement('div');
-placeholder.style.height = div.offsetHeight + 'px';
-placeholder.style.display = 'none';
-div.parentNode.insertBefore(placeholder, div);
-const threshold = div.offsetTop;
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > threshold) {
-    div.classList.add('fixed-top');
-    placeholder.style.display = 'block'; // show the placeholder element when the div element becomes fixed
-  } else {
-    div.classList.remove('fixed-top');
-    placeholder.style.display = 'none'; // hide the placeholder element when the div element is no longer fixed
-  }
-});
-const divmenuspan = document.querySelector('.divmenuspan');
-const dropdiv = document.querySelector('.dropdiv');
-divmenuspan.addEventListener('click', () => {
-  console.log('Нажал');
-  dropdiv.style.height = "68vw";
-  setTimeout(function () {
-    dropdiv.style.padding = "3vw";
-  }, 60);
-  // dropdiv.style.padding = "3vw";
-});
-const dropdivb1 = document.querySelector('.dropdiv .a1 .b1');
-dropdivb1.addEventListener('click', () => {
-  console.log('Нажал');
-  dropdiv.style.height = "0";
-  // dropdiv.style.padding = "0";
-  setTimeout(function () {
-    dropdiv.style.padding = "0";
-  }, 240);
-});
-// window.addEventListener('load', function () {
-//   if (window.matchMedia("(orientation: landscape)").matches) {
-//     // экран стал горизонтальным
-//     this.location.href = "index.html"
-//     console.log("Экран стал горизонтальным");
+// const div = document.querySelector('.headerdiv');
+// const placeholder = document.createElement('div');
+// placeholder.style.height = div.offsetHeight + 'px';
+// placeholder.style.display = 'none';
+// div.parentNode.insertBefore(placeholder, div);
+// const threshold = div.offsetTop;
+// window.addEventListener('scroll', () => {
+//   if (window.pageYOffset > threshold) {
+//     div.classList.add('fixed-top');
+//     placeholder.style.display = 'block'; // show the placeholder element when the div element becomes fixed
+//   } else {
+//     div.classList.remove('fixed-top');
+//     placeholder.style.display = 'none'; // hide the placeholder element when the div element is no longer fixed
 //   }
 // });
-window.addEventListener("resize", function () {
+// const divmenuspan = document.querySelector('.divmenuspan');
+// const dropdiv = document.querySelector('.dropdiv');
+// divmenuspan.addEventListener('click', () => {
+//   console.log('Нажал');
+//   dropdiv.style.height = "68vw";
+//   setTimeout(function () {
+//     dropdiv.style.padding = "3vw";
+//   }, 60);
+//   // dropdiv.style.padding = "3vw";
+// });
+// const dropdivb1 = document.querySelector('.dropdiv .a1 .b1');
+// dropdivb1.addEventListener('click', () => {
+//   console.log('Нажал');
+//   dropdiv.style.height = "0";
+//   // dropdiv.style.padding = "0";
+//   setTimeout(function () {
+//     dropdiv.style.padding = "0";
+//   }, 240);
+// });
+// // window.addEventListener('load', function () {
+// //   if (window.matchMedia("(orientation: landscape)").matches) {
+// //     // экран стал горизонтальным
+// //     this.location.href = "index.html"
+// //     console.log("Экран стал горизонтальным");
+// //   }
+// // });
+// window.addEventListener("resize", function () {
 
-  if (window.matchMedia("(orientation: portrait)").matches) {
-    // экран стал вертикальным
-    if (this.document.body.offsetWidth < 480) {
-      this.location.href = "index.html";
-    }
-  }
-});
+//   if (window.matchMedia("(orientation: portrait)").matches) {
+//     // экран стал вертикальным
+//     if (this.document.body.offsetWidth < 480) {
+//       this.location.href = "index.html";
+//     }
+//   }
+// });
